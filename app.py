914d9904794d4de3631d62c2fb80aa2fb5ef9702
yeOmaNnn <cool.rtj2016@yandex.ru>
2026-05-24 19:35:20 +0300

    add werkzeug

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def handle_disconnect():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, log_output=None, use_reloader=None)
+    socketio.run(app, host="0.0.0.0", port=port, log_output=None, use_reloader=None, allow_unsafe_werkzeug=True)
